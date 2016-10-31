1236c8da69df67807b0d1412651f1376b1fe7a3e
puja agarwal <agarwal.puja3001@gmail.com>
2016-10-31 18:14:06 +0800

    added jsf pages

--- ejava_CA2/src/java/ejava/ca2/view/LoginView.java
+++ ejava_CA2/src/java/ejava/ca2/view/LoginView.java
@@ -1,14 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ejava.ca2.view;
 
-/**
- *
- * @author agarwal.puja
- */
-public class LoginView {
-    
+import java.io.Serializable;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import javax.servlet.http.HttpServletRequest;
+
+@ViewScoped
+@Named
+public class LoginView implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private String username;
+	private String password;
+
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String login() {
+		HttpServletRequest req = 
+				(HttpServletRequest)FacesContext.getCurrentInstance()
+						.getExternalContext().getRequest();
+		try {
+			req.login(username, password);
+		} catch (Throwable t) {
+			FacesContext.getCurrentInstance()
+					.addMessage(null, new FacesMessage("Incorrect login"));
+			return (null);
+		}
+
+		return ("secure/menu");
+	}
+
 }
+
