5b7637c8ff5aca6f3f8eabb78535606d67cdfc35
puja agarwal <agarwal.puja3001@gmail.com>
2016-11-01 17:44:59 +0800

    login done-added jsf pages

---
+++ ejava_CA2/src/java/ejava/ca2/business/NoticeServlet.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejava.ca2.business;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet(urlPatterns = {"/secure/notices", "/notices"})
+public class NoticeServlet extends HttpServlet {
+    
+    @Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
+			throws ServletException, IOException {
+            
+            String noticeURL = req.getContextPath() + "/faces/notices.html";
+            resp.sendRedirect(noticeURL);
+                
+	}
+}
