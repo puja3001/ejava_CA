043d2794a1f7f6e8bce078b1a85afab369461be7
puja agarwal <agarwal.puja3001@gmail.com>
2016-11-07 14:50:20 +0800

    CA3 initial upload done

--- ejava_CA3/src/java/ejava/ca3/web/UploadServlet.java
+++ ejava_CA3/src/java/ejava/ca3/web/UploadServlet.java
@@ -9,6 +9,7 @@ import ejava.ca3.business.PodBean;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Collection;
 import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
@@ -16,6 +17,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
 /**
  *
@@ -30,12 +32,16 @@ public class UploadServlet extends HttpServlet{
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException{
         
-        BufferedReader reader = new BufferedReader(new InputStreamReader(
-            req.getInputStream()));
-            StringBuilder sb = new StringBuilder();
-            for (String line; (line = reader.readLine()) != null;) {
-             System.out.println(line);
-  }
+        
+        String note = req.getPart("note").toString();
+        System.out.println("note: "+note);
+        
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(
+//            req.getInputStream()));
+//            StringBuilder sb = new StringBuilder();
+//            for (String line; (line = reader.readLine()) != null;) {
+//             System.out.println(line);
+//  }
         
 //        int podId = Integer.parseInt(req.getParameter("podId"));
 //        String note = req.getParameter("note");
